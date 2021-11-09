87ea23eb7c39d27ad9c6340b6428742a40d7f7b6
Quandong-Zhang <78526012+Quandong-Zhang@users.noreply.github.com>
2021-11-09 09:07:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import webbrowser
 import keyboard
 import time
 
-def fish(url):
-    webbrowser.open(url, new=1, autoraise=True)
-    keyboard.wait("esc")
-    os.system("taskkill /IM chrome.exe")
-    time.sleep(3)
-    os.system("taskkill /F /IM chrome.exe")
 
-if __name__=="__maun__":
-    fish("https://www.bilibili.com/bangumi/play/ss39424?t=246")
+webbrowser.open("https://www.bilibili.com/bangumi/play/ss39424?t=246", new=1)
+keyboard.wait("alt")
+os.system("taskkill /IM chrome.exe")
+time.sleep(2.5)
+os.system("taskkill /F /IM chrome.exe")
