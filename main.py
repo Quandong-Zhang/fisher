efc32d8a2effe46cba20832eda362fe581669883
Quandong-Zhang <78526012+Quandong-Zhang@users.noreply.github.com>
2021-11-09 08:54:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import os
+import webbrowser
+import keyboard
+import time
+
+def fish(url):
+    webbrowser.open(url, new=1, autoraise=True)
+    keyboard.wait("esc")
+    os.system("taskkill /IM chrome.exe")
+    time.sleep(3)
+    os.system("taskkill /F /IM chrome.exe")
