74a398472cfc5ce007cb4588a51394288691c724
Quandong-Zhang <78526012+Quandong-Zhang@users.noreply.github.com>
2021-11-09 09:53:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import keyboard
 #import time
 
 
-webbrowser.open("https://www.bilibili.com/bangumi/play/ss39424?t=246", new=1)
+webbrowser.open("https://www.bilibili.com/bangumi/play/ss39424", new=1)
 keyboard.wait("alt")
 os.system("taskkill /IM chrome.exe")
 #time.sleep(2.5)
