caf2d7f264263896c4ba8a7a8124228c9333aaf9
Quandong-Zhang <78526012+Quandong-Zhang@users.noreply.github.com>
2021-11-09 09:17:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import webbrowser
 import keyboard
-import time
+#import time
 
 
 webbrowser.open("https://www.bilibili.com/bangumi/play/ss39424?t=246", new=1)
 keyboard.wait("alt")
 os.system("taskkill /IM chrome.exe")
-time.sleep(2.5)
-os.system("taskkill /F /IM chrome.exe")
+#time.sleep(2.5)
+#os.system("taskkill /F /IM chrome.exe")
